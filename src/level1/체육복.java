559615eea8bf542d9a681ae223c9a5751f29ead1
jun111haha <jun111haha@gmail.com>
2022-02-16 00:19:40 +0900

    [KJ]02.16

--- src/level1/체육복.java
+++ src/level1/체육복.java
@@ -30,7 +30,7 @@ public class 체육복 {
 
         for(int i=0; i<lost.length; i++){
             for(int j=0; j<reserve.length; j++){
-                if((lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j])){
+                if(lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]){
                     answer++;
                     reserve[j] = -1;
                     break;
@@ -38,6 +38,7 @@ public class 체육복 {
             }
         }
 
+        System.out.println(answer);
         return answer;
     }
 
@@ -45,7 +46,7 @@ public class 체육복 {
     public static void main(String[] args) {
         int n = 5;
         int [] lost = {2,4};
-        int [] reserve = {1,3,5};
+        int [] reserve = {3};
 
 
         solution(n , lost, reserve);
