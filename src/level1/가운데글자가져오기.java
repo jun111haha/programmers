9875694c7f1e76a29dbc0a50536738a47c5dfe40
kj <jun111haha@gmail.com>
2022-02-23 00:27:51 +0900

    22.02.22 kongkong

--- src/level1/가운데글자가져오기.java
+++ src/level1/가운데글자가져오기.java
@@ -1,5 +1,9 @@
 package level1;
 
+/*
+* 글자가 짝수면 substring API 활용 / 2 한 후, 전 index 부터 잘라서 answer 에 담아준다.
+* 글자가 홀수면 subsrting API 활용 / 2 한 후, 뒤에 +1 하여 잘라서 answer 에 담아준다.
+* */
 public class 가운데글자가져오기 {
 
     public static String solution(String s) {
@@ -10,8 +14,6 @@ public class 가운데글자가져오기 {
             answer = s.substring(s.length() / 2 - 1, s.length() / 2 +1);
         }else{
             answer = s.substring(s.length() / 2, s.length() / 2 +1);
-
-
         }
 
         System.out.println(answer);
--- src/level1/문자열p와y개수.java
+++ src/level1/문자열p와y개수.java
@@ -1,5 +1,10 @@
 package level1;
 
+/*
+* 소문자로 전부 변경후
+* count 변수 선언
+* 분기처리 -> 카운트 증감 / 감소
+* */
 public class 문자열p와y개수 {
 
     public static boolean solution(String s) {
